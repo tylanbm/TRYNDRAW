10f8eaa938645de013f3d161b66683b9fa985225
Tyrell Bain <tyrellbain44@gmail.com>
2021-12-05 11:26:30 -0800

    Merged 'origin/main' with codehome

--- App.js
+++ App.js
@@ -3,10 +3,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { getAuth, signOut, onAuthStateChanged } from "firebase/auth";
-<<<<<<< HEAD
-=======
-import { useState } from 'react';
->>>>>>> origin/main
 
 
 //Screen Imports
@@ -22,7 +18,6 @@ const Tab = createBottomTabNavigator();
 const auth = getAuth();
 
 
-<<<<<<< HEAD
 onAuthStateChanged(auth, (user) => {
   if (user) {
     // User is signed in, see docs for a list of available properties
@@ -81,44 +76,6 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator>
       {global.signedIn ? (
-=======
-const AppWithTabs = () => (
-  <Tab.Navigator>
-    <Tab.Screen name="Home2" component={HomeScreen} />
-    <Tab.Screen name="Details" component={DetailsScreen} />
-  </Tab.Navigator>
-
-);
-
-function App() {
-  //Keeps state of whether user is signed in or not
-  const [isSignedIn, setIsSignedIn] = useState('')
-  
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/firebase.User
-      const uid = user.uid;
-      setIsSignedIn(true);
-      console.log("User currently signed in")
-
-      return true;
-      // ...
-    } else {
-      console.log("User not signed in")
-      setIsSignedIn(false);
-      return false;
-
-      
-    }
-  });
-
- //If user is signed in then render the app with tabs, otherwise send user to log in screen
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-      {isSignedIn ? (
->>>>>>> origin/main
         <>
           <Stack.Screen name="Home" component={AppWithTabs} />
         </>
@@ -128,7 +85,6 @@ function App() {
             <Stack.Screen name="SignUp" component={SignUpScreen} />
           </>
   )}
-<<<<<<< HEAD
       </Stack.Navigator>
     </NavigationContainer>
   
@@ -155,13 +111,6 @@ function App() {
     </NavigationContainer>
 */
 
-=======
-      </Stack.Navigator>
-    </NavigationContainer>
-  
-  );
-}
->>>>>>> origin/main
 
 
 export default App;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,19 +2,15 @@ import React from 'react'
 import { StyleSheet, Text, View, Button } from 'react-native'
 import { getAuth, signOut,onAuthStateChanged } from "firebase/auth";
 
-<<<<<<< HEAD
 
 
 
-=======
->>>>>>> origin/main
 function HomeScreen({navigation }) {
     
 
 
 
 
-<<<<<<< HEAD
  const signOutUser = () => {
      signOut(auth).then(() => {
         // Sign-out successful.
@@ -46,18 +42,6 @@ function HomeScreen({navigation }) {
         }
     });
     
-=======
-    const signOutUser = () => {
-        signOut(auth).then(() => {
-            // Sign-out successful.
-            console.log("Signed Out")
-            global.signedIn = false;
-        }).catch((error) => {
-            // An error happened.
-        });
-    }
-
->>>>>>> origin/main
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Home Screen</Text>
