e9d7c8088668f1ed992e43af9ab4e4eb5ccf757a
tylanm <tylanmcpherson@gmail.com>
2021-12-19 11:14:05 -0800

    Added test canvas screen and removed canvas pkg

---
+++ screens/TestCanvasScreen.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+
+const TestCanvasScreen = () => {
+    return (
+        <View style={styles.container}>
+            
+        </View>
+    )
+}
+
+export default TestCanvasScreen
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#F5FCFF',
+    },
+});
