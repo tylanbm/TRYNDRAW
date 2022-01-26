806c3a00b12c210f05dc453f82e2fa10b8f7b221
tylanbm <tylanmcpherson@gmail.com>
2022-01-26 13:05:14 -0800

    Image screen styled, added buttons

---
+++ components/IonButton.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React from 'react';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const IonButton = (props) => {
+  return (
+      
+      <TouchableOpacity onPress={props.onPress} style={props.style}>
+          <Ionicons name={props.name} size={24} color={props.color} />
+      </TouchableOpacity>
+  );
+};
+
+export default IonButton;
+
+const styles = StyleSheet.create({});
--- screens/ImageScreen.js
+++ screens/ImageScreen.js
@@ -1,6 +1,20 @@
 import { StyleSheet, Text, View, TouchableOpacity,Image, Dimensions } from 'react-native';
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
+import IonButton from '../components/IonButton';
+
+
+const likeImage = () => {
+    console.log("You liked the image");
+}
+
+const dislikeImage = () => {
+    console.log("You disliked the image");
+}
+
+const reportImage = () => {
+    console.log("You reported the image");
+}
 
 const { width: screenWidth, height: screenHeight } = Dimensions.get('window');
 
@@ -11,15 +25,32 @@ const ImageScreen = ({route, navigation}) => {
   
   return (
     <View>
-        <Text>Username of Author</Text>
-        
-        <TouchableOpacity onPress={() => console.log("Touched photo")}>
+        <View style={styles.imageHeaderContainer}>
             <Image
-                source={{ uri: imageSourceToLoad }} style={styles.image}
+                source={{ uri: imageSourceToLoad }} style={styles.authorProfilePhoto}
             />
-        </TouchableOpacity>
-        <Text>Like Button</Text>
-        <Text>Dislike Button</Text>
+            <Text style={styles.authorNameText}>Username of Author</Text>
+        </View>
+        
+        <View style={styles.imageContainer}>
+            <TouchableOpacity onPress={() => console.log("Touched photo")}>
+                <Image
+                    source={{ uri: imageSourceToLoad }} style={styles.image}
+                />
+            </TouchableOpacity>
+        </View>
+
+        <View style={styles.imageFooterContainer}>
+              <View style={styles.buttonContainer}>
+                <IonButton name="heart" onPress={() => likeImage()} color="gray" style={styles.buttonContainer} />
+              </View>
+              <View style={styles.buttonContainer}>
+                  <IonButton name="thumbs-down" onPress={() => dislikeImage()} color="gray" />
+              </View>
+              <View style={styles.reportButtonContainer}>
+                  <IonButton name="flag" onPress={() => reportImage()} color="red" />
+              </View>
+        </View>
 
         <Text>Comments</Text>
     </View>
@@ -29,8 +60,48 @@ const ImageScreen = ({route, navigation}) => {
 export default ImageScreen;
 
 const styles = StyleSheet.create({
+    imageContainer: {
+        borderTopWidth: 0.5,
+        borderBottomWidth: 0.5,
+        borderTopColor: 'gray',
+        borderBottomColor: "gray",
+    },
     image: {
+
         width:screenWidth,
-        height:screenWidth,
+        height:screenWidth+2,
+    },
+    imageHeaderContainer: {
+        flexDirection: 'row',
+        alignContent: 'center',
+        alignItems: 'center',
+    },
+    authorProfilePhoto: {
+        marginLeft: 4,
+        marginVertical: 4,
+        borderRadius: 30,
+        width: 36,
+        height: 36,
+        borderWidth: 1,
+        borderColor: 'black',
+    },
+    authorNameText: {
+        marginLeft: 8,
+        fontSize: 14,
+        fontWeight: 'bold',
+    },
+    imageFooterContainer: {
+        flexDirection: 'row',
+        alignContent: 'center',
+        alignItems: 'center',
+        flexGrow: 1,
+    },
+    buttonContainer: {
+        padding: 4,
+    },
+    reportButtonContainer: {
+        padding: 4,
+        marginLeft: "auto"
     }
+
 });
